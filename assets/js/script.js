bf7b5ba0895c39835794bfc1cf6edf2185238520
Jonnywdev <jonnywhitt19@gmail.com>
2022-07-27 16:47:30 +0000

    Update homescreen stylees

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const navGrow = document.querySelector('.extend-nav');
+const navToggle = document.querySelector('#nav-toggle');
+
+
+navToggle.addEventListener('click', () => {
+    navGrow.classList.toggle('show-mobile-nav');
+})
