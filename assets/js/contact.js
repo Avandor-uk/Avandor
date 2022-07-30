ed369278ce23fcea3123ee218057ea231e78bc05
Jonnywdev <jonnywhitt19@gmail.com>
2022-07-30 08:27:08 +0000

    Add new js file in attempt to fix menu bug

---
+++ assets/js/contact.js
@@ -0,0 +1,9 @@
+const contactForm = document.querySelector('#contact-form');
+
+
+contactForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+});
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,6 @@
 const navGrow = document.querySelector('.extend-nav');
 const navToggle = document.querySelector('#nav-toggle');
-const contactForm = document.querySelector('#contact-form');
 
-
-contactForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    e.target.elements.name.value = '';
-    e.target.elements.email.value = '';
-    e.target.elements.message.value = '';
-});
 
 
 navToggle.addEventListener('click', () => {
